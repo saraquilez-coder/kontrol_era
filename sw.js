bebff6745775e15efdcd46e71860aeb3e4c5dacb
saraquilez-coder <saraquilez@gmail.com>
2026-04-21 13:53:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kontrol-era-v4';
+const CACHE_NAME = 'kontrol-era-v5';
 const assets = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', e => {
